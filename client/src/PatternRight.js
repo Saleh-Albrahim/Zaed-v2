34b1559ce2a0d0b575f13daee5fa1c97275365ab
AnasMG <49666154+Anas-MG@users.noreply.github.com>
2021-05-12 18:27:27 +0300

    Delete client directory

--- client/src/PatternRight.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Image, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
-import pattern from './images/pattern-Right.png';
-
-export const PatternRight = props => {
-  return <Image src={pattern} {...props} />;
-};
--- client/src/pages/BeneficiaryPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { ChakraProvider } from '@chakra-ui/react';
-
-function BeneficiaryPage() {
-  return <ChakraProvider></ChakraProvider>;
-}
-
-export default BeneficiaryPage;
